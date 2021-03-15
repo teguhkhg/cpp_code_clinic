4cb79585bce483437cc32e5c5b0dec148ae72987
teguhkhg <teguhkhg@gmail.com>
2021-03-15 14:26:13 +0700

    clean up

--- aspect_oriented/include/plus/wrap_plus.hpp
+++ aspect_oriented/include/plus/wrap_plus.hpp
@@ -1,26 +1,8 @@
 #include <iostream>
 #include <functional>
 
-// Change lambda expression to std::function
-template <typename Function>
-struct function_traits 
-: public function_traits<decltype(&Function::operator())>
-{};
+#include <utils/to_function.hpp>
 
-template <typename ClassType, typename ReturnType, typename ...Args>
-struct function_traits<ReturnType(ClassType::*)(Args...) const>
-{
-    typedef ReturnType (*pointer)(Args...);
-    typedef std::function<ReturnType(Args...)> function;
-};
-
-template <typename Function>
-typename function_traits<Function>::function
-to_function(Function &lambda){
-    return typename function_traits<Function>::function(lambda);
-}
-
-// log wrapper
 template <typename ...Args>
 std::function<void(Args...)> wrapLog(std::function<void(Args...)> f){
     return [f](Args... args){
